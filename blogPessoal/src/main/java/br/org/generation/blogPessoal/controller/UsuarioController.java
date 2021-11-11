173a90a3b5d533484daace440717c99657d5dfcf
novaavos <avosjogos@gmail.com>
2021-11-11 17:04:49 -0300

    anti-dup fix + collections + ratio

--- blogPessoal/src/main/java/br/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/br/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,11 +1,13 @@
 package br.org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,11 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAllUsuario(){
+		return ResponseEntity.ok(usuarioService.listarUsuarios());
+	}
+	
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
 		return usuarioService.Logar(user)
@@ -30,10 +37,14 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
+	
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+	public ResponseEntity<Usuario> postUsuario(@RequestBody Usuario usuario){
+		
+		return usuarioService.CadastrarUsuario(usuario)
+				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(resp))
+				.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+		
 	}
 	
 	
--- blogPessoal/src/main/java/br/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/br/org/generation/blogPessoal/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.org.generation.blogPessoal.service;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
@@ -16,8 +17,9 @@ import br.org.generation.blogPessoal.repository.UsuarioRepository;
 public class UsuarioService {
 
 	@Autowired
-	private UsuarioRepository repository;
-	
+	private UsuarioRepository repositoryUsuario;
+	/*
+	//Usar um optinal para ele conseguir retornar um nulo quando o usuário ficar repetido
 	public Usuario CadastrarUsuario(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
@@ -27,10 +29,27 @@ public class UsuarioService {
 		return repository.save(usuario);
 		
 	}
+	*/
+	public List<Usuario> listarUsuarios(){
+		return repositoryUsuario.findAll();
+	}
+	
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		if(repositoryUsuario.findByUsuario(usuario.getUsuario()).isPresent()) return Optional.empty();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		
+		usuario.setSenha(senhaEncoder);
+		
+		return Optional.of(repositoryUsuario.save(usuario));
+		
+	}
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin> user){
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();	
-		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		Optional<Usuario> usuario = repositoryUsuario.findByUsuario(user.get().getUsuario());
 		
 		if(usuario.isPresent()) {
 			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
@@ -39,6 +58,7 @@ public class UsuarioService {
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
 				
+			 
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
 				
@@ -46,7 +66,7 @@ public class UsuarioService {
 			}
 		}
 		
-		return null;
+		return Optional.empty();
 	}
 	
 }
